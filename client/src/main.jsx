5bee7af283ee56a25bf8cd7ed049f988a7f714c3
HuynhLongNhat <nhathuynh227@gmail.com>
2024-07-01 12:41:59 +0700

    Cấu hình react router dom v6

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { RouterProvider } from "react-router-dom"
+import Router from './Router/index.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Router}>
+      <App />
+
+    </RouterProvider>
   </React.StrictMode>,
 )
