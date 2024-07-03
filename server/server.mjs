d57d22405b7fb0ff254b5c2a5fb4e023fe29c87c
HuynhLongNhat <nhathuynh227@gmail.com>
2024-07-03 13:59:20 +0700

    Kết nối database MongoDB

--- server/server.mjs
+++ server/server.mjs
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express, { query } from "express"
 import http from "http"
 import { ApolloServer } from "@apollo/server"
@@ -5,38 +6,25 @@ import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHt
 import bodyParser from "body-parser"
 import { expressMiddleware } from "@apollo/server/express4"
 import cors from "cors"
-
+import mongoose from "mongoose";
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { typeDefs } from './schemas/index.js';
+import { resolvers } from './resolvers/index.js';
 const app = express()
 const httpServer = http.createServer(app)
 
-const typeDefs = `#graphql
-
-type Query {
-   folders : [Folder]
-}
 
 
-type Mutation
-  {
 
-    }
+//connect to DB
+const URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cnl5wm8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const PORT = process.env.PORT || 4000
 
-type Subscription {
 
-}
-`;
+const schema = makeExecutableSchema({ typeDefs });
 
-const resolvers = {
-  Query: {
-    name: () => {
-
-    }
-  }
-
-}
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 })
 
@@ -44,5 +32,14 @@ await server.start()
 
 app.use(cors(), bodyParser.json(), expressMiddleware(server))
 
-await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve))
-console.log("Server ready at port  : 4000")
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    console.log('Connected to DB');
+    await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+    console.log('🚀 Server ready at http://localhost:4000');
+  });
